895f4f9f41f7b3be6254eaf00a0a164f1a09e334
MAT-2 <mpcasey30@gmail.com>
2023-10-08 16:25:13 -0500

    Added JS for inputting city name to button event listener.

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,33 @@
+var cityInputEl = document.querySelector("#cityInput");
+var searchBtn = document.querySelector("#search-btn");
 var APIKey = "64c9a35de2c16fb58dacd57d4783c3f5";
-var city = "Chicago";
-var queryURL =
-  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  city +
-  "&appid=" +
-  APIKey;
+var city;
+// var queryURL =
+//   "https://api.openweathermap.org/data/2.5/weather?q=" +
+//   city +
+//   "&appid=" +
+//   APIKey;
 
-fetch(queryURL);
+//JS Injection example
+var queryURL = `https://api.openweathermap.org/data/2.5/weather?q= 
+  ${city}
+  &appid=
+  ${APIKey}`;
+
+function getAPI() {
+  city = cityInputEl.value;
+  console.log(city);
+  // fetch(queryURL)
+  //   .then(function (response) {
+  //     return response.json();
+  //   })
+  //   .then(function (data) {
+  //     console.log(data);
+  //   });
+}
+
+console.log(getAPI());
 
 console.log(queryURL);
+
+searchBtn.addEventListener("click", getAPI);
