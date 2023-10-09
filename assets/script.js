c79a15e1420a8e870a1a7376086d2a7f62316cc8
MAT-2 <mpcasey30@gmail.com>
2023-10-08 19:12:05 -0500

    Added more variables and IDs to populate data for wind, humidity, and city name to be displayed.

--- assets/script.js
+++ assets/script.js
@@ -3,20 +3,19 @@ var searchBtn = document.querySelector("#search-btn");
 var APIKey = "64c9a35de2c16fb58dacd57d4783c3f5";
 var city;
 var tempEl = document.querySelector("#temp");
-// var queryURL =
-//   "https://api.openweathermap.org/data/2.5/weather?q=" +
-//   city +
-//   "&appid=" +
-//   APIKey;
-
-//JS Injection example
+var windEl = document.querySelector("#wind");
+var humidityEl = document.querySelector("#humidity");
+var cityEl = document.querySelector("#city-name");
 
 function getAPI() {
   city = cityInputEl.value;
   console.log(city);
 
+  //JS Injection example shown here, concatenating strings in JS.
+
   var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKey}&units=imperial`;
 
+  //Need to fetch the data from queryURL. THEN return it as JS. THEN return data to be used.
   fetch(queryURL)
     .then(function (response) {
       return response.json();
@@ -25,6 +24,9 @@ function getAPI() {
       if (data) {
         console.log(data);
         tempEl.textContent = data.main.temp + "F";
+        windEl.textContent = data.wind.speed + "MPH";
+        humidityEl.textContent = data.main.humidity + "%";
+        cityEl.textContent = data.name;
       }
     });
 }
